57030d94392913485a0c94459de211dbfb47a90f
okramnesseam <bgh264@myy.haaga-helia.fi>
2020-03-10 11:44:53 +0200

    Update number something

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,6 +1,7 @@
 package com.example.bookstore.web;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -28,6 +30,19 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+    
+	// RESTful service to get all students !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get student by id !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111
+    @RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }       
+    
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
@@ -42,13 +57,14 @@ public class BookController {
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
-        return "redirect:booklist";
+        return "redirect:../booklist";
     }    
     @RequestMapping(value = "/modify/{id}", method = RequestMethod.GET)
     public String modifyBook(@PathVariable("id") Long bookId, Model model) {
     	Optional<Book> book = repository.findById(bookId);
         model.addAttribute("categories", crepository.findAll());
     	model.addAttribute("book", book);
+    	System.out.println("GGGGGGG");
         return "modifybook";
     }    
 }
