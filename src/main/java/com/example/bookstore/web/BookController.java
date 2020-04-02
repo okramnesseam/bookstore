9f2b224072eb2e73d4588dee0f72b4ccbd51f20b
okramnesseam <bgh264@myy.haaga-helia.fi>
2020-04-02 19:43:05 +0300

    testing stuff

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,7 +4,12 @@ package com.example.bookstore.web;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -72,5 +77,13 @@ public class BookController {
 	public String login() {
 		return "login";
 	}
-    
+ 
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+     }
 }
--- src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.bookstore.web.BookController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController controller;
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+	assertThat(controller).isNotNull();
 	}
-
-}
+	}
